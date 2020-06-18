211768bb23bdfb923336c03198282b8d8dda56cf
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-06-18 15:45:45 +0800

    简易界面

--- TransverterApp/TransverterApp/MainPage.xaml.cs
+++ TransverterApp/TransverterApp/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using Xamarin.Essentials;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace TransverterApp
 {
@@ -13,9 +17,37 @@ namespace TransverterApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<Label> fileList = new ObservableCollection<Label>();
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void ReadFiles(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            if (bt.Text == "读取")
+            {
+                fileList.Clear();
+                string basepath = Environment.GetEnvironmentVariable("ANDROID_STORAGE");
+                string dirpath = Path.Combine(basepath, "emulated", "0", "qqmusic", "song");
+                if(true)
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(dirpath);
+                    foreach (var f in dirInfo.GetFiles())
+                    {
+                        Label lb = new Label();
+                        lb.Text = f.Name;
+                        fileList.Add(lb);
+                    }
+                }
+                bt.Text = "转换";
+            }
+            else
+            {
+                bt.Text = "读取";
+            }
         }
     }
 }
